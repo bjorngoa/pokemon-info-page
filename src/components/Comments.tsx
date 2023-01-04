6edf9c907a38d3aec0383d2b8edec7e676fe8504
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-01-04 22:41:59 +0100

    comment component on a seperate tsx file

---
+++ src/components/Comments.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export const Comments = () => {
+ // const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const [comments, setComments] = useState<string[]>([])
+  const [comment, setComment] = useState("")
+
+  const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => { 
+    e.preventDefault()
+    setComments(oldComments => [...oldComments, comment])
+    setComment("")
+  }
+
+  console.log(comments)
+  // FIXME: make last comment submited show at the top 
+  return (
+    <>
+        <form className="flex gap-4" onSubmit={handleSubmit}>
+          <textarea value={comment} onChange={e => setComment(e.target.value)}  placeholder= "Add a comment..." />
+          <button type="submit" disabled={comment.length < 1}>Comment</button>
+        </form>
+
+        
+        <section className="bg-white w-2/3 mt-4 ">
+            {comments.map((comment, idx) => <div className="border-b-gray-600-700 border"  key={idx}><p className="text-black">{comment}</p></div>)}
+        </section>
+
+    </>
+  );
+};
+
+export default Comments;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,10 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { useState } from "react";
+import Comments from "../components/Comments";
 
 const Home: NextPage = () => {
  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
-  const [comments, setComments] = useState<string[]>([])
-  const [comment, setComment] = useState("")
 
-  const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => { 
-    e.preventDefault()
-    setComments(oldComments => [...oldComments, comment])
-  }
-  const CommentSection = () => {
-    return (
-      comments.map(comment => <p key={comment}>{comment}</p>)
-    )
-  }
-  console.log(comments)
   
   return (
     <>
@@ -26,12 +14,9 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <CommentSection />
-        <form className="flex gap-4" onSubmit={handleSubmit}>
-          
-          <textarea value={comment} onChange={e => setComment(e.target.value)}  placeholder="Add a comment.."/>
-          <button type="submit" >Comment</button>
-        </form>
+       
+       
+        <Comments />
       </main>
     </>
   );
